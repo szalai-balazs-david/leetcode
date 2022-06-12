8ed1a97405cbc6a3ea64247791c89a9e1eca28d1
Balazs <balazs@altaeros.com>
2022-06-12 16:44:08 +0200

    Add solution

--- 0031 Next Permutation/Solution.py
+++ 0031 Next Permutation/Solution.py
@@ -1,3 +1,29 @@
 class Solution:
-    def method(self):
-        print("test")
+    def nextPermutation(self, nums: list[int]) -> None:
+        last_increasing = -1
+        for i in range(len(nums) - 1):
+            if nums[i] < nums[i + 1]:
+                last_increasing = i
+
+        if last_increasing == -1:
+            nums.sort()
+            return
+
+        elements = nums[last_increasing:]
+
+        if len(elements) == 2:
+            nums[last_increasing] = elements[1]
+            nums[last_increasing + 1] = elements[0]
+            return
+
+        old = elements[0]
+        new = elements[1]
+        for e in elements[2:]:
+            if new - old > e - old > 0:
+                new = e
+        nums[last_increasing] = new
+
+        elements.remove(new)
+        elements.sort()
+        for i in range(last_increasing + 1, len(nums)):
+            nums[i] = elements[i - (last_increasing + 1)]
--- 0031 Next Permutation/test_Solution.py
+++ 0031 Next Permutation/test_Solution.py
@@ -3,6 +3,37 @@ from Solution import Solution
 
 
 class TestSolution(TestCase):
-    def test_method(self):
+    def test_nextPermutation1(self):
+        nums = [1, 2, 3]
+        expected = [1,3,2]
         x = Solution()
-        self.fail()
+        x.nextPermutation(nums)
+        self.assertListEqual(expected, nums)
+
+    def test_nextPermutation2(self):
+        nums = [3,2,1]
+        expected = [1,2,3]
+        x = Solution()
+        x.nextPermutation(nums)
+        self.assertListEqual(expected, nums)
+
+    def test_nextPermutation3(self):
+        nums = [1, 1, 5]
+        expected = [1,5,1]
+        x = Solution()
+        x.nextPermutation(nums)
+        self.assertListEqual(expected, nums)
+
+    def test_nextPermutation4(self):
+        nums = [1, 3, 2]
+        expected = [2,1,3]
+        x = Solution()
+        x.nextPermutation(nums)
+        self.assertListEqual(expected, nums)
+
+    def test_nextPermutation5(self):
+        nums = [2, 3, 1]
+        expected = [3,1,2]
+        x = Solution()
+        x.nextPermutation(nums)
+        self.assertListEqual(expected, nums)
