3aa9b6880ceb744fca2bb4efa142595f933b8fea
Balazs <balazs@altaeros.com>
2022-06-12 16:15:47 +0200

    Add implementation

--- 0030 Substring with Concatenation of All Words/Solution.py
+++ 0030 Substring with Concatenation of All Words/Solution.py
@@ -1,3 +1,36 @@
+import re
+
+
 class Solution:
     def findSubstring(self, s: str, words: list[str]) -> list[int]:
-        return []
+        result = []
+
+        indices = {}
+        for word in words:
+            if word in indices.values():
+                continue
+            locations = [m.start() for m in re.finditer(f"(?={word})", s)]
+            for location in locations:
+                indices[location] = word
+        word_length = len(words[0])
+
+        for i in range(len(s) - len(words) * word_length + 1):
+
+            if indices.get(i) is None:
+                continue
+            expected_indexes = []
+            for j in range(len(words)):
+                expected_indexes.append(i + j * word_length)
+            remaining_words = words.copy()
+
+            for expected_index in expected_indexes:
+                if indices.get(expected_index) is not None:
+                    if remaining_words.__contains__(indices[expected_index]):
+                        remaining_words.remove(indices[expected_index])
+                    else:
+                        break
+            if len(remaining_words) == 0:
+                result.append(i)
+
+        return result
+    
