ea93d6297edc319a33309410ac26be85ac63fd84
Balazs <balazs@altaeros.com>
2022-06-13 18:27:03 +0200

    Add solution with cheating after looking at solution

--- 0032 Longest Valid Parentheses/Solution.py
+++ 0032 Longest Valid Parentheses/Solution.py
@@ -1,27 +1,15 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        result = 0
+        array = [0]
+        for i in range(1, len(s)):
+            sub_result = 0
+            if s[i] == ")":
+                if s[i-1] == "(":
+                    sub_result = (array[i-2] if i >= 2 else 0) + 2
+                elif i - array[i-1] > 0 and s[i - array[i-1] - 1] == "(":
+                    sub_result = array[i - 1] + (array[i - array[i - 1] - 2] if (i - array[i - 1]) >= 2 else 0) + 2
 
-        array = []
-        for c in s:
-            array.append(1 if c == "(" else -1)
+            array.append(sub_result)
 
-        i = 0
-        while i < len(s):
-            current = [0]
-            for j in range(i, len(array)):
-                current.append(current[-1] + array[j])
-                if current[-1] < 0:
-                    break
-            i += 1
-            if -1 not in current:
-                j = 1
-                while j < len(current) and current[j] not in current[j+1:]:
-                    j += 1
-                    i += 1
+        return max(array)
 
-            current.reverse()
-            sub_result = len(current) - current.index(0) - 1
-            result = max(result, sub_result)
-
-        return result
