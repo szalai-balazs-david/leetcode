e255d35bf5eb564ace42c0cd4cf6817595defe97
Balazs <balazs@altaeros.com>
2022-06-13 12:45:11 +0200

    Add finalized version

--- 0027 Remove Element/Solution.py
+++ 0027 Remove Element/Solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
-        nums.sort()
         for i in range(len(nums) - 1, -1, -1):
-            if nums[i] < val:
-                break
             if nums[i] == val:
                 del nums[i]
         return len(nums)
+        
