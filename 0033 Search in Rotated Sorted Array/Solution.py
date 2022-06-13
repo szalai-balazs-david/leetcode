3afd80c53e461bb0b83b41b488c0aecfb25c4be7
Balazs <balazs@altaeros.com>
2022-06-13 13:35:38 +0200

    Add first solution

--- 0033 Search in Rotated Sorted Array/Solution.py
+++ 0033 Search in Rotated Sorted Array/Solution.py
@@ -1,4 +1,58 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        return 0
-    
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+
+        pivot = self.unrotate(nums)
+        if pivot == -1:
+            values = nums.copy()
+        else:
+            values = nums[pivot + 1:] + nums[:pivot + 1]
+
+        if values[0] > target or values[-1] < target:
+            return -1
+
+        result = 0
+        while True:
+            if len(values) == 1:
+                result = result + 1 + pivot
+                if result >= len(nums):
+                    result -= len(nums)
+                return result if values[0] == target else -1
+            half = int(len(values) / 2)
+            x = values[:half]
+            y = values[half:]
+            if x[-1] == target:
+                result = result + len(x) + pivot
+                if result >= len(nums):
+                    result -= len(nums)
+                return result
+            if x[-1] < target:
+                if y[0] > target:
+                    return -1
+                result += len(x)
+                values = y
+            else:
+                values = x
+
+    def unrotate(self, nums: list[int]) -> int:
+        if nums[0] < nums[-1]:
+            return -1
+
+        values = nums.copy()
+        pivot = 0
+
+        while True:
+            half = int(len(values) / 2)
+            x = values[:half]
+            y = values[half:]
+            if x[0] <= x[-1] and y[0] <= y[-1]:
+                pivot += len(x)
+                break
+            elif x[0] <= x[-1]:
+                pivot += len(x)
+                values = y
+            else:
+                values = x
+
+        return pivot - 1
--- 0033 Search in Rotated Sorted Array/test_Solution.py
+++ 0033 Search in Rotated Sorted Array/test_Solution.py
@@ -13,4 +13,32 @@ class TestSolution(TestCase):
 
     def test_search3(self):
         x = Solution()
+        self.assertEqual(7, x.search([4,5,6,7,8,9,10,0,1,2], 0))
+
+    def test_search4(self):
+        x = Solution()
+        self.assertEqual(3, x.search([4,5,6,7,8,9,10,0,1,2], 7))
+
+    def test_search5(self):
+        x = Solution()
         self.assertEqual(-1, x.search([1], 0))
+
+    def test_search6(self):
+        x = Solution()
+        self.assertEqual(-1, x.search([1,3], 0))
+
+    def test_search7(self):
+        x = Solution()
+        self.assertEqual(1, x.search([1,3], 3))
+
+    def test_search8(self):
+        x = Solution()
+        self.assertEqual(0, x.search([3,1], 3))
+
+    def test_search9(self):
+        x = Solution()
+        self.assertEqual(-1, x.search([3,5,1], 0))
+
+    def test_unrotate1(self):
+        x = Solution()
+        self.assertEqual(3, x.unrotate([4,5,6,7,0,1,2]))
