a3dc7b23e83b5b3ff27642525010d9fd5eae3ca6
Balazs <balazs@altaeros.com>
2022-06-18 16:05:53 +0200

    Add first solution

--- 0034 Find First and Last Position of Element in Sorted Array/Solution.py
+++ 0034 Find First and Last Position of Element in Sorted Array/Solution.py
@@ -1,3 +1,52 @@
 class Solution:
     def searchRange(self, nums: list[int], target: int) -> list[int]:
-        return [-2, -2]
+        if len(nums) == 0 or target > nums[-1] or target < nums[0]:
+            return [-1, -1]
+
+        first = self.find_first(nums, target)
+        if first == -1:
+            return [-1, -1]
+
+        return [first, self.find_last(nums, target)]
+
+    def find_first(self, nums: list[int], target) -> int:
+        if nums[0] == target:
+            return 0
+
+        left = 0
+        right = len(nums) - 1
+        mid_prev = -1
+
+        while left < right:
+            mid = int((left + right + 1) / 2)
+            print(f"first: left: {left}, right: {right}, mid: {mid}")
+            if nums[mid] == target and nums[mid - 1] != target:
+                return mid
+            if nums[mid] >= target:
+                right = mid
+            else:
+                left = mid
+            if mid_prev == mid:
+                return -1
+            mid_prev = mid
+
+    def find_last(self, nums: list[int], target) -> int:
+        if nums[-1] == target:
+            return len(nums) - 1
+
+        left = 0
+        right = len(nums) - 1
+        mid_prev = -1
+
+        while left < right:
+            mid = int((left + right) / 2)
+            print(f"last: left: {left}, right: {right}, mid: {mid}")
+            if nums[mid] == target and nums[mid + 1] != target:
+                return mid
+            if nums[mid] <= target:
+                left = mid
+            else:
+                right = mid
+            if mid_prev == mid:
+                return -1
+            mid_prev = mid
--- 0034 Find First and Last Position of Element in Sorted Array/test_Solution.py
+++ 0034 Find First and Last Position of Element in Sorted Array/test_Solution.py
@@ -14,3 +14,11 @@ class TestSolution(TestCase):
     def test_searchRange3(self):
         x = Solution()
         self.assertListEqual([-1, -1], x.searchRange([], 0))
+
+    def test_searchRange4(self):
+        x = Solution()
+        self.assertListEqual([6,6], x.searchRange([1,3,7,7,8,8,10,11], 10))
+
+    def test_searchRange5(self):
+        x = Solution()
+        self.assertListEqual([2,5], x.searchRange([1,2,3,3,3,3,4,5,9],3))
