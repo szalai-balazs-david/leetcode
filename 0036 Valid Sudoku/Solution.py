c25e6193e63564c0c092b7e1e2769d3c7e3d08ae
Balazs <balazs@altaeros.com>
2022-06-19 15:49:45 +0200

    Add improved solution for task 36

--- 0036 Valid Sudoku/Solution.py
+++ 0036 Valid Sudoku/Solution.py
@@ -1,32 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
         for row in board:
-            if not self.isValidGroup(row):
+            if self.isBad(row):
                 return False
         for i in range(9):
             column = []
             for x in range(9):
                 column.append(board[x][i])
-            if not self.isValidGroup(column):
+            if self.isBad(column):
                 return False
         for i in range(3):
             for j in range(3):
                 group = []
                 for x in range(3):
-                    for y in range(3):
-                        group.append(board[i * 3 + x][j * 3 + y])
-                if not self.isValidGroup(group):
+                    group = group + board[i * 3 + x][j * 3 : j * 3 + 3]
+                print(group)
+                if self.isBad(group):
                     return False
 
         return True
 
-    def isValidGroup(self, nums: list[int]) -> bool:
+    def isBad(self, nums: list[int]) -> bool:
         found = []
         for x in nums:
             if x == ".":
                 continue
             if x in found:
-                return False
+                return True
             found.append(x)
 
-        return True
+        return False
