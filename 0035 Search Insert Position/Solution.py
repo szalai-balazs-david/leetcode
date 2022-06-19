3cd56238cd59a909fecc24bf3729f1312ff98f81
Balazs <balazs@altaeros.com>
2022-06-19 15:08:09 +0200

    Add 1st solution to task 35

--- 0035 Search Insert Position/Solution.py
+++ 0035 Search Insert Position/Solution.py
@@ -1,3 +1,20 @@
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
-        return -1
+        if nums[0] >= target:
+            return 0
+        if nums[-1] < target:
+            return len(nums)
+        if nums[-1] == target:
+            return len(nums) - 1
+
+        left = 0
+        right = len(nums) - 1
+
+        while True:
+            mid = int((left + right + 1) / 2)
+            if nums[mid] >= target > nums[mid - 1]:
+                return mid
+            if nums[mid] > target:
+                right = mid
+            else:
+                left = mid
--- 0035 Search Insert Position/test_Solution.py
+++ 0035 Search Insert Position/test_Solution.py
@@ -14,3 +14,27 @@ class TestSolution(TestCase):
     def test_searchInsert3(self):
         x = Solution()
         self.assertEqual(4, x.searchInsert([1,3,5,6], 7))
+
+    def test_searchInsert4(self):
+        x = Solution()
+        self.assertEqual(7, x.searchInsert([1,3,5,6,8,9,100], 101))
+
+    def test_searchInsert5(self):
+        x = Solution()
+        self.assertEqual(0, x.searchInsert([1,3,5,6,8,9,100], 0))
+
+    def test_searchInsert6(self):
+        x = Solution()
+        self.assertEqual(0, x.searchInsert([1], 0))
+
+    def test_searchInsert7(self):
+        x = Solution()
+        self.assertEqual(0, x.searchInsert([1], 1))
+
+    def test_searchInsert8(self):
+        x = Solution()
+        self.assertEqual(1, x.searchInsert([1], 2))
+
+    def test_searchInsert9(self):
+        x = Solution()
+        self.assertEqual(1, x.searchInsert([1,3], 3))
