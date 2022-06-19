88bc482c1fcf0a7fd23ff6127ee0bee7ea4864c2
Balazs <balazs@altaeros.com>
2022-06-19 15:19:04 +0200

    Add recursive solution to task 35

--- 0035 Search Insert Position/Solution.py
+++ 0035 Search Insert Position/Solution.py
@@ -1,20 +1,13 @@
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
-        if nums[0] >= target:
+        if len(nums) == 0:
             return 0
-        if nums[-1] < target:
-            return len(nums)
-        if nums[-1] == target:
-            return len(nums) - 1
+        if len(nums) == 1:
+            return 0 if nums[0] >= target else 1
 
-        left = 0
-        right = len(nums) - 1
-
-        while True:
-            mid = int((left + right + 1) / 2)
-            if nums[mid] >= target > nums[mid - 1]:
-                return mid
-            if nums[mid] > target:
-                right = mid
-            else:
-                left = mid
+        mid = int(len(nums) / 2)
+        if nums[mid] == target:
+            return mid
+        if nums[mid] > target:
+            return self.searchInsert(nums[:mid], target)
+        return mid + self.searchInsert(nums[mid:], target)
