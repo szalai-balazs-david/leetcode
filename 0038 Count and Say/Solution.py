e1a5535f649466b3b31662825c92448caee52d52
Balazs <balazs@altaeros.com>
2022-06-19 18:15:35 +0200

    Add 1st solution to task 38

--- 0038 Count and Say/Solution.py
+++ 0038 Count and Say/Solution.py
@@ -1,3 +1,18 @@
 class Solution:
-    def method(self):
-        print("test")
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+
+        say = self.countAndSay(n - 1)
+
+        result = ""
+        i = 0
+        while i < len(say):
+            num = say[i]
+            j = i
+            while j < len(say) and say[j] == say[i]:
+                j += 1
+            result += str(j - i) + str(num)
+            i = j
+
+        return result
--- 0038 Count and Say/test_Solution.py
+++ 0038 Count and Say/test_Solution.py
@@ -3,6 +3,14 @@ from Solution import Solution
 
 
 class TestSolution(TestCase):
-    def test_method(self):
+    def test_countAndSay1(self):
         x = Solution()
-        self.fail()
+        self.assertEqual("1", x.countAndSay(1))
+
+    def test_countAndSay2(self):
+        x = Solution()
+        self.assertEqual("1211", x.countAndSay(4))
+
+    def test_countAndSay3(self):
+        x = Solution()
+        self.assertEqual("111221", x.countAndSay(5))
