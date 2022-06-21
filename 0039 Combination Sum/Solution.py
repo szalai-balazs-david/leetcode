b33085dfb76a49693bc6f9e389a81da383cb89c5
Balazs <balazs@altaeros.com>
2022-06-21 17:41:46 +0200

    Add solution to task 39
    Had to look at a solution to eliminate an error.
    Had most of it right, just missed a speed optimization step.

--- 0039 Combination Sum/Solution.py
+++ 0039 Combination Sum/Solution.py
@@ -1,3 +1,20 @@
 class Solution:
-    def method(self):
-        print("test")
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        result = []
+        candidates.sort()
+
+        for i in range(len(candidates)):
+            candidate = candidates[i]
+            if candidate > target:
+                continue
+            if candidate == target:
+                result.append([candidate])
+            else:
+                sub_results = self.combinationSum(candidates[i:], target - candidate)
+                for sub_result in sub_results:
+                    sub_result.append(candidate)
+                    sub_result.sort()
+                    if sub_result not in result:
+                        result.append(sub_result)
+
+        return result
--- 0039 Combination Sum/test_Solution.py
+++ 0039 Combination Sum/test_Solution.py
@@ -3,6 +3,31 @@ from Solution import Solution
 
 
 class TestSolution(TestCase):
-    def test_method(self):
+    def test_combinationSum1(self):
         x = Solution()
-        self.fail()
+        expected = [[2,2,3],[7]]
+        actual = x.combinationSum([2,3,6,7], 7)
+        self.assertEqual(len(expected), len(actual))
+        for i in range(len(actual)):
+            self.assertTrue(actual[i] in expected)
+
+    def test_combinationSum2(self):
+        x = Solution()
+        expected = [[2,2,2,2],[2,3,3],[3,5]]
+        actual = x.combinationSum([2,3,5], 8)
+        self.assertEqual(len(expected), len(actual))
+        for i in range(len(actual)):
+            self.assertTrue(actual[i] in expected)
+
+    def test_combinationSum3(self):
+        x = Solution()
+        expected = []
+        actual = x.combinationSum([2], 1)
+        self.assertEqual(len(expected), len(actual))
+        for i in range(len(actual)):
+            self.assertTrue(actual[i] in expected)
+
+    def test_combinationSum4(self):
+        #testing for timeout
+        x = Solution()
+        x.combinationSum([4, 12, 100, 200], 400)
