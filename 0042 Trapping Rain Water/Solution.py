cc6fa55645aae6b8b825755287ec5e7b22dcc374
Balazs <balazs@altaeros.com>
2022-06-22 14:23:44 +0200

    Add overcomplicated solution to task 42

--- 0042 Trapping Rain Water/Solution.py
+++ 0042 Trapping Rain Water/Solution.py
@@ -1,3 +1,37 @@
 class Solution:
-    def method(self):
-        print("test")
+    def trap(self, height: list[int]) -> int:
+        if len(height) < 3:
+            return 0
+        i_max = height.index(max(height))
+        result = self.trapLeft(height[:i_max + 1]) + self.trapRight(height[i_max:])
+
+        return result
+
+    def trapRight(self, height: list[int]) -> int:
+        if len(height) < 3:
+            return 0
+
+        result = 0
+        right = len(height) - 1
+        for i in range(len(height)-1, -1, -1):
+            if height[i] >= height[right]:
+                print(height[i])
+                for val in height[i + 1 : right]:
+                    result += height[right] - val
+                right = i
+
+        return result
+
+    def trapLeft(self, height: list[int]) -> int:
+        if len(height) < 3:
+            return 0
+
+        result = 0
+        left = 0
+        for i in range(len(height)):
+            if height[i] >= height[left]:
+                for val in height[left + 1 : i]:
+                    result += height[left] - val
+                left = i
+
+        return result
--- 0042 Trapping Rain Water/test_Solution.py
+++ 0042 Trapping Rain Water/test_Solution.py
@@ -3,6 +3,14 @@ from Solution import Solution
 
 
 class TestSolution(TestCase):
-    def test_method(self):
+    def test_trap1(self):
         x = Solution()
-        self.fail()
+        self.assertEqual(6, x.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+
+    def test_trap2(self):
+        x = Solution()
+        self.assertEqual(9, x.trap([4,2,0,3,2,5]))
+
+    def test_trap3(self):
+        x = Solution()
+        self.assertEqual(1, x.trap([4,2,3]))
