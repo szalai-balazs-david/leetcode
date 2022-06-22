1a4a705e64d24093fc0aa4f84b8bb8f9d0e786b4
Balazs <balazs@altaeros.com>
2022-06-22 15:36:34 +0200

    Add solution that's too slow

--- 0044 Wildcard Matching/Solution.py
+++ 0044 Wildcard Matching/Solution.py
@@ -1,3 +1,13 @@
 class Solution:
-    def method(self):
-        print("test")
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+        if not s:
+            return not p or (p[0] == "*" and self.isMatch(s, p[1:]))
+
+        if p[0] == "*":
+            if len(p) > 1 and p[1] == "*":
+                return self.isMatch(s, p[1:])
+            return self.isMatch(s, p[1:]) or self.isMatch(s[1:], p)
+        else:
+            return bool(s) and p[0] in [s[0], "?"] and self.isMatch(s[1:], p[1:])
--- 0044 Wildcard Matching/test_Solution.py
+++ 0044 Wildcard Matching/test_Solution.py
@@ -3,6 +3,66 @@ from Solution import Solution
 
 
 class TestSolution(TestCase):
-    def test_method(self):
+    def test_regex_match1(self):
         x = Solution()
-        self.fail()
+        self.assertFalse(x.isMatch("aa", "a"))
+
+    def test_regex_match2(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("aa", "*"))
+
+    def test_regex_match3(self):
+        x = Solution()
+        self.assertFalse(x.isMatch("cb", "?a"))
+
+    def test_regex_match4(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("abccced", "a?*d"))
+
+    def test_regex_match5(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("abd", "a?*d"))
+
+    def test_regex_match6(self):
+        x = Solution()
+        self.assertFalse(x.isMatch("abcccd", "a?*dd"))
+
+    def test_regex_match7(self):
+        x = Solution()
+        self.assertFalse(x.isMatch("abcccd", "a*dd"))
+
+    def test_regex_match8(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("abcccde", "a*de"))
+
+    def test_regex_match9(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("abcccde", "a*e"))
+
+    def test_regex_match10(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("aaa", "*a"))
+
+    def test_regex_match11(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("mississippi", "mi*i**?"))
+
+    def test_regex_match12(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("aabcbcbcaccbcaabc", "*a*a*?**?***"))
+
+    def test_regex_match13(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("abcaaaaaaabaabcabac", "*ab?a******"))
+
+    def test_regex_match14(self):
+        x = Solution()
+        self.assertTrue(x.isMatch("abcd", "*"))
+
+    def test_regex_match15(self):
+        x = Solution()
+        self.assertFalse(x.isMatch("babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab","***bba**a*bbba**aab**b"))
+
+    def test_regex_match16(self):
+        x = Solution()
+        self.assertFalse(x.isMatch("babbbbaabababaabbababaababaabbaabababbaaababbababaaaaaabbabaaaabababbabbababbbaaaababbbabbbbbbbbbbaabbb","b**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a"))
