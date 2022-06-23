9f4c1a76b52761c105b5208d23318d7d55dfd146
Balazs <balazs@altaeros.com>
2022-06-23 20:12:36 +0200

    Add decent solution to task 106

--- 0106 Construct Binary Tree from Inorder and Postorder Traversal/Solution.py
+++ 0106 Construct Binary Tree from Inorder and Postorder Traversal/Solution.py
@@ -1,3 +1,21 @@
+from Utilities.TreeNode import TreeNode
+from typing import Optional
+
+
 class Solution:
-    def method(self):
-        print("test")
+    def buildTree(self, inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
+        match len(inorder):
+            case 0:
+                return None
+            case 1:
+                return TreeNode(inorder[0])
+            case 2:
+                if inorder[0] == postorder[0]:
+                    return TreeNode(inorder[1], TreeNode(inorder[0], None))
+                return TreeNode(inorder[0], None, TreeNode(inorder[1]))
+
+        i = inorder.index(postorder[-1])
+
+        return TreeNode(postorder[-1],
+                        self.buildTree(inorder[:i], postorder[:i]),
+                        self.buildTree(inorder[i + 1:], postorder[i: -1]))
--- 0106 Construct Binary Tree from Inorder and Postorder Traversal/test_Solution.py
+++ 0106 Construct Binary Tree from Inorder and Postorder Traversal/test_Solution.py
@@ -1,8 +1,26 @@
 from unittest import TestCase
 from Solution import Solution
+from Utilities.TreeNode import treeNode2List
 
 
 class TestSolution(TestCase):
-    def test_method(self):
+    def test_buildTree1(self):
         x = Solution()
-        self.fail()
+        expected = [3,9,20,None,None,15,7]
+        node = x.buildTree([9,3,15,20,7], [9,15,7,20,3])
+        actual = treeNode2List(node)
+        self.assertListEqual(expected, actual)
+
+    def test_buildTree2(self):
+        x = Solution()
+        expected = [-1]
+        node = x.buildTree([-1], [-1])
+        actual = treeNode2List(node)
+        self.assertListEqual(expected, actual)
+
+    def test_buildTree3(self):
+        x = Solution()
+        expected = [1,2,None]
+        node = x.buildTree([2,1], [2,1])
+        actual = treeNode2List(node)
+        self.assertListEqual(expected, actual)
