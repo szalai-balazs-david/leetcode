ef333bae291ad0c1912b16365abddebaab793945
Balazs <balazs@altaeros.com>
2022-06-23 12:06:55 +0200

    Add faster solution to task 44
    Still too slow

--- 0044 Wildcard Matching/Solution.py
+++ 0044 Wildcard Matching/Solution.py
@@ -2,12 +2,32 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         if not p:
             return not s
-        if not s:
-            return not p or (p[0] == "*" and self.isMatch(s, p[1:]))
+        if p == "*":
+            return True
 
-        if p[0] == "*":
-            if len(p) > 1 and p[1] == "*":
-                return self.isMatch(s, p[1:])
-            return self.isMatch(s, p[1:]) or self.isMatch(s[1:], p)
+        c = p[0]
+        if c == "*":
+            # There's at least 1 more letter in p
+            if p[1] == "*":
+                match = self.isMatch(s, p[1:])
+                return match
+            if p[1] == "?":
+                match = self.isMatch(s, p[1:]) or (len(s) > 0 and self.isMatch(s[1:], p))
+                return match
+            section = ""
+            for c in p[1:]:
+                if c not in ["*", "?"]:
+                    section += c
+                else:
+                    break
+            if section in s:
+                i = s.index(section)
+                match = self.isMatch(s[i + len(section):], p[1 + len(section):]) or self.isMatch(s[i + 1:], p)
+                return match
+            return False
         else:
-            return bool(s) and p[0] in [s[0], "?"] and self.isMatch(s[1:], p[1:])
+            if not s:
+                return False
+            is_match = p[0] in [s[0], "?"]
+            match = is_match and self.isMatch(s[1:], p[1:])
+            return match
