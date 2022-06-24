254c4a8b2c919549c9b5239db6d4b39fb29d4042
Balazs <balazs@altaeros.com>
2022-06-24 10:19:07 +0200

    Add decent solution to task 45

--- 0045 Jump Game II/Solution.py
+++ 0045 Jump Game II/Solution.py
@@ -1,3 +1,25 @@
 class Solution:
     def jump(self, nums: list[int]) -> int:
-        return -1
+        print(nums)
+        if len(nums) == 1:
+            return 0
+        if nums[0] >= len(nums) - 1:
+            return 1
+
+        def singleJump(index: int) -> int:
+            best = 0
+            best_index = index
+            for i in range(1, nums[index] + 1):
+                current = i + nums[i + index]
+                if current >= best:
+                    best = current
+                    best_index = i + index
+            return best_index
+
+        i = 0
+        count = 1
+        while nums[i] < len(nums) - 1 - i:
+            count += 1
+            i = singleJump(i)
+
+        return count
--- 0045 Jump Game II/test_Solution.py
+++ 0045 Jump Game II/test_Solution.py
@@ -10,3 +10,11 @@ class TestSolution(TestCase):
     def test_jump2(self):
         x = Solution()
         self.assertEqual(2, x.jump([2,3,0,1,4]))
+
+    def test_jump3(self):
+        x = Solution()
+        self.assertEqual(6, x.jump([2,3,0,1,4,1,3,2,1,2,1,3,2,4,1]))
+
+    def test_jump4(self):
+        x = Solution()
+        self.assertEqual(0, x.jump([0]))
