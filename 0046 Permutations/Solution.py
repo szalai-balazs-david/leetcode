e11512c04f8fa63dca7d90c4ab3432c70c239050
Balazs <balazs@altaeros.com>
2022-06-24 11:17:17 +0200

    Add first solution to task 46

--- 0046 Permutations/Solution.py
+++ 0046 Permutations/Solution.py
@@ -1,3 +1,13 @@
 class Solution:
-    def method(self):
-        print("test")
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        if len(nums) == 1:
+            return [nums]
+        result = []
+
+        x = nums[0]
+        y = self.permute(nums[1:])
+        for i in range(len(nums)):
+            for yy in y:
+                arr = yy[:i] + [x] + yy[i:]
+                result.append(arr)
+        return result
--- 0046 Permutations/test_Solution.py
+++ 0046 Permutations/test_Solution.py
@@ -3,6 +3,26 @@ from Solution import Solution
 
 
 class TestSolution(TestCase):
-    def test_method(self):
+    def test_permute1(self):
         x = Solution()
-        self.fail()
+        expected = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+        actual = x.permute([1,2,3])
+        self.assertEqual(len(expected), len(actual))
+        for x in expected:
+            self.assertTrue(x in actual)
+
+    def test_permute2(self):
+        x = Solution()
+        expected = [[0,1],[1,0]]
+        actual = x.permute([0,1])
+        self.assertEqual(len(expected), len(actual))
+        for x in expected:
+            self.assertTrue(x in actual)
+
+    def test_permute3(self):
+        x = Solution()
+        expected = [[1]]
+        actual = x.permute([1])
+        self.assertEqual(len(expected), len(actual))
+        for x in expected:
+            self.assertTrue(x in actual)
