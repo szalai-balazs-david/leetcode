a1d4f694f07499842304098b61d038d2443aa001
Balazs <balazs@altaeros.com>
2022-06-24 11:30:04 +0200

    Add slightly improved solution to task 46

--- 0046 Permutations/Solution.py
+++ 0046 Permutations/Solution.py
@@ -6,8 +6,14 @@ class Solution:
 
         x = nums[0]
         y = self.permute(nums[1:])
-        for i in range(len(nums)):
+        for yy in y:
+            arr = [x] + yy
+            result.append(arr)
+        for i in range(1, len(nums)-1):
             for yy in y:
                 arr = yy[:i] + [x] + yy[i:]
                 result.append(arr)
+        for yy in y:
+            arr = yy + [x]
+            result.append(arr)
         return result
