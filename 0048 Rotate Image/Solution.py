a6fe007f63ae074ebefd784c4322700d5781cfc7
Balazs <balazs@altaeros.com>
2022-07-13 13:31:29 +0200

    Add slighty improved solution

--- 0048 Rotate Image/Solution.py
+++ 0048 Rotate Image/Solution.py
@@ -5,17 +5,17 @@ class Solution:
 
         for i in range(layers):
             if len(matrix) % 2 == 0:
+                core = center - i - 1
+                nCore = -(core + 1)
                 for j in range(2 * i + 1):
-                    core = center - i - 1
-                    nCore = -(core + 1)
                     pos = center + i - j
                     nPos = -(pos + 1)
                     matrix[core][pos], matrix[nPos][core], matrix[nCore][nPos], matrix[pos][nCore] = \
                         matrix[nPos][core], matrix[nCore][nPos], matrix[pos][nCore], matrix[core][pos]
             else:
+                core = center - i - 1
+                nCore = -(core + 1)
                 for j in range(2 * (i + 1)):
-                    core = center - i - 1
-                    nCore = -(core + 1)
                     pos = center + i - j
                     nPos = -(pos + 1)
                     matrix[core][pos], matrix[nPos][core], matrix[nCore][nPos], matrix[pos][nCore] = \
