f49def93781d5409d11e4dea6019f252a944e0ed
Balazs <balazs@altaeros.com>
2022-07-14 12:01:59 +0200

    Add dynamic programming solution to task 53

--- 0053 Maximum Subarray/Solution.py
+++ 0053 Maximum Subarray/Solution.py
@@ -1,3 +1,15 @@
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
-        return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        current_max = max(nums)
+        current_sum = 0
+        for i in range(len(nums)):
+            if current_sum + nums[i] < 0:
+                current_sum = 0
+            else:
+                current_sum = current_sum + nums[i]
+                current_max = max(current_max, current_sum)
+
+        return current_max
