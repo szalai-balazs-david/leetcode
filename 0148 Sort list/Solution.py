4664235a293973dd4e4e033281b04d4ad930db68
Balazs <balazs@altaeros.com>
2022-07-14 15:32:11 +0200

    Add first functional solution to task 148 (timelimit exceeded)

--- 0148 Sort list/Solution.py
+++ 0148 Sort list/Solution.py
@@ -1,7 +1,21 @@
-from Utilities.ListNode import ListNode
+from Utilities.ListNode import ListNode, printListNode
 from typing import Optional
 
 
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return None
+        if head is None or head.next is None:
+            return head
+
+        current = head
+        while current.next is not None:
+            point = current
+            lowest = current
+            while point is not None:
+                if lowest.val > point.val:
+                    lowest = point
+                point = point.next
+            current.val, lowest.val = lowest.val, current.val
+            current = current.next
+
+        return head
