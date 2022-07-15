5d873498bd7da3ad0143ec03388f0fb5420dab2f
Balazs <balazs@altaeros.com>
2022-07-15 12:03:20 +0200

    Add solution to task 54 (beats 99 / 82%)

--- 0054 Spiral Matrix/Solution.py
+++ 0054 Spiral Matrix/Solution.py
@@ -1,3 +1,19 @@
 class Solution:
     def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
-        return []
+        result = []
+        h = len(matrix)
+        w = len(matrix[0])
+
+        for i in range(min(h + 1, w + 1) // 2):
+            result += matrix[i][i:w-i]
+            for j in range(i + 1, h - 1 - i):
+                result.append(matrix[j][-1-i])
+            if i != h - i - 1:
+                last_row = matrix[-1-i][i:w-i]
+                last_row.reverse()
+                result += last_row
+            if i != w - i - 1:
+                for j in range(h - 2 - i, i, -1):
+                    result.append(matrix[j][i])
+
+        return result
--- 0054 Spiral Matrix/test_Solution.py
+++ 0054 Spiral Matrix/test_Solution.py
@@ -10,3 +10,7 @@ class TestSolution(TestCase):
     def test_spiral_order2(self):
         x = Solution()
         self.assertListEqual([1,2,3,4,8,12,11,10,9,5,6,7], x.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+
+    def test_spiral_order3(self):
+        x = Solution()
+        self.assertListEqual([7,9,6], x.spiralOrder([[7],[9],[6]]))
