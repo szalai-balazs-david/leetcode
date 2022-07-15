3abf53c7e8972dde5635e6b76b92a175d40973ff
Balazs <balazs@altaeros.com>
2022-07-15 11:42:30 +0200

    Add baseline to task 44

---
+++ 0054 Spiral Matrix/test_Solution.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from Solution import Solution
+
+
+class TestSolution(TestCase):
+    def test_spiral_order1(self):
+        x = Solution()
+        self.assertListEqual([1,2,3,6,9,8,7,4,5], x.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+
+    def test_spiral_order2(self):
+        x = Solution()
+        self.assertListEqual([1,2,3,4,8,12,11,10,9,5,6,7], x.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
