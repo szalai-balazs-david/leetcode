b83d22bd36afcc3ff3761fd54b7f3f394589ffc6
Balazs <balazs@altaeros.com>
2022-07-16 11:30:16 +0200

    Add first solutions to task 102

--- 0102 Binary Level order Traversal/Solution.py
+++ 0102 Binary Level order Traversal/Solution.py
@@ -4,4 +4,25 @@ from typing import Optional
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
-        return []
+        if root is None:
+            return []
+
+        elements = [root]
+        result = []
+        while len(elements) > 0:
+            children = []
+            level = []
+
+            for element in elements:
+                if element is None:
+                    continue
+                level.append(element.val)
+                if element.left is not None:
+                    children.append(element.left)
+                if element.right is not None:
+                    children.append(element.right)
+
+            result.append(level)
+            elements = children
+
+        return result
