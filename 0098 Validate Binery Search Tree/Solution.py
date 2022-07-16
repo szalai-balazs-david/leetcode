4df01ebd91a030a9942aa39a0fa8c3e0811f4b3c
Balazs <balazs@altaeros.com>
2022-07-16 10:47:11 +0200

    Add baseline for task 98

---
+++ 0098 Validate Binery Search Tree/Solution.py
@@ -0,0 +1,7 @@
+from Utilities.TreeNode import TreeNode
+from typing import Optional
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return False
---
+++ 0098 Validate Binery Search Tree/test_Solution.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from Solution import Solution
+from Utilities.TreeNode import TreeNode
+
+
+class TestSolution(TestCase):
+    def test_is_valid_bst1(self):
+        x = Solution()
+        tree = TreeNode(2,
+                        TreeNode(1),
+                        TreeNode(3))
+        self.assertEqual(True, x.isValidBST(tree))
+
+    def test_is_valid_bst2(self):
+        x = Solution()
+        tree = TreeNode(5,
+                        TreeNode(1),
+                        TreeNode(4,
+                                 TreeNode(3),
+                                 TreeNode(6)))
+        self.assertEqual(False, x.isValidBST(tree))
