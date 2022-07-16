caef4cab1bce4383712018c0a1ae6efc01c03b27
Balazs <balazs@altaeros.com>
2022-07-16 12:06:11 +0200

    Add improved solution to task 55

--- 0055 Jump Game/Solution.py
+++ 0055 Jump Game/Solution.py
@@ -2,15 +2,14 @@ class Solution:
     def canJump(self, nums: list[int]) -> bool:
         if 0 not in nums:
             return True
-        nums.reverse()
-        return self.test(nums[1:], 0)
+        return self.test(nums[:-1], 0)
 
     def test(self, nums: list[int], start: int) -> bool:
         try:
             index = nums.index(0, start)
             canDo = False
-            for i in range(index + 1, len(nums), 1):
-                if nums[i] > i - index:
+            for i in range(index - 1, -1, -1):
+                if nums[i] > index - i:
                     canDo = True
                     break
             if not canDo:
--- 0055 Jump Game/test_Solution.py
+++ 0055 Jump Game/test_Solution.py
@@ -18,3 +18,7 @@ class TestSolution(TestCase):
     def test_can_jump4(self):
         x = Solution()
         self.assertEqual(False, x.canJump([0,1]))
+
+    def test_can_jump5(self):
+        x = Solution()
+        self.assertEqual(True, x.canJump([2,3,1,1,4,0]))
