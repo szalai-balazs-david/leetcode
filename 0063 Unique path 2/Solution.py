68ffcfb9c884ca4d37deeeda121ad6adefec32fd
Balazs <balazs@altaeros.com>
2022-07-17 13:33:14 +0200

    Add slightly improved solution to task 63

--- 0063 Unique path 2/Solution.py
+++ 0063 Unique path 2/Solution.py
@@ -3,18 +3,17 @@ class Solution:
         if obstacleGrid[0][0] == 1 or obstacleGrid[len(obstacleGrid) - 1][len(obstacleGrid[0]) - 1] == 1:
             return 0
         grid = [[0]*len(obstacleGrid[0]) for i in range(len(obstacleGrid))]
-        for i in range(0, len(obstacleGrid)):
-            for j in range(0, len(obstacleGrid[0])):
+
+        grid[0][0] = 1 - obstacleGrid[0][0]
+        for i in range(1, len(obstacleGrid)):
+            grid[i][0] = 0 if grid[i-1][0] == 0 else 1 - obstacleGrid[i][0]
+        for j in range(1, len(obstacleGrid[0])):
+            grid[0][j] = 0 if grid[0][j-1] == 0 else 1 - obstacleGrid[0][j]
+
+        for i in range(1, len(obstacleGrid)):
+            for j in range(1, len(obstacleGrid[0])):
                 if obstacleGrid[i][j] == 1:
                     grid[i][j] = 0
-                elif i == 0:
-                    if j == 0:
-                        grid[i][j] = 1
-                    else:
-                        grid[i][j] = grid[i][j-1]
                 else:
-                    if j == 0:
-                        grid[i][j] = grid[i-1][j]
-                    else:
-                        grid[i][j] = grid[i-1][j] + grid[i][j-1]
+                    grid[i][j] = grid[i-1][j] + grid[i][j-1]
         return grid[len(obstacleGrid) - 1][len(obstacleGrid[0]) - 1]
