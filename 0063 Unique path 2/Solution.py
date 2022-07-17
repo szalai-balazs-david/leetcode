972dc2e822ae116baf4ed97e56a1719403832725
Balazs <balazs@altaeros.com>
2022-07-17 13:26:01 +0200

    Add first solution to task 63

--- 0063 Unique path 2/Solution.py
+++ 0063 Unique path 2/Solution.py
@@ -1,3 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
-        return 0
+        if obstacleGrid[0][0] == 1 or obstacleGrid[len(obstacleGrid) - 1][len(obstacleGrid[0]) - 1] == 1:
+            return 0
+        grid = [[0]*len(obstacleGrid[0]) for i in range(len(obstacleGrid))]
+        for i in range(0, len(obstacleGrid)):
+            for j in range(0, len(obstacleGrid[0])):
+                if obstacleGrid[i][j] == 1:
+                    grid[i][j] = 0
+                elif i == 0:
+                    if j == 0:
+                        grid[i][j] = 1
+                    else:
+                        grid[i][j] = grid[i][j-1]
+                else:
+                    if j == 0:
+                        grid[i][j] = grid[i-1][j]
+                    else:
+                        grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        return grid[len(obstacleGrid) - 1][len(obstacleGrid[0]) - 1]
--- 0063 Unique path 2/test_Solution.py
+++ 0063 Unique path 2/test_Solution.py
@@ -10,3 +10,15 @@ class TestSolution(TestCase):
     def test_unique_paths_with_obstacles2(self):
         x = Solution()
         self.assertEqual(1, x.uniquePathsWithObstacles([[0,1],[0,0]]))
+
+    def test_unique_paths_with_obstacles3(self):
+        x = Solution()
+        self.assertEqual(0, x.uniquePathsWithObstacles([[0,0],[0,1]]))
+
+    def test_unique_paths_with_obstacles4(self):
+        x = Solution()
+        self.assertEqual(0, x.uniquePathsWithObstacles([[1,0],[0,0]]))
+
+    def test_unique_paths_with_obstacles5(self):
+        x = Solution()
+        self.assertEqual(0, x.uniquePathsWithObstacles([[0,0],[1,1],[0,0]]))
