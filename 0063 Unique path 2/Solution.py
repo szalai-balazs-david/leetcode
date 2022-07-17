deb6bb8c048dc50bc8c9d4df86394b44945cc338
Balazs <balazs@altaeros.com>
2022-07-17 13:37:07 +0200

    Add improved solution to task 63

--- 0063 Unique path 2/Solution.py
+++ 0063 Unique path 2/Solution.py
@@ -4,11 +4,16 @@ class Solution:
             return 0
         grid = [[0]*len(obstacleGrid[0]) for i in range(len(obstacleGrid))]
 
-        grid[0][0] = 1 - obstacleGrid[0][0]
-        for i in range(1, len(obstacleGrid)):
-            grid[i][0] = 0 if grid[i-1][0] == 0 else 1 - obstacleGrid[i][0]
-        for j in range(1, len(obstacleGrid[0])):
-            grid[0][j] = 0 if grid[0][j-1] == 0 else 1 - obstacleGrid[0][j]
+        for i in range( len(obstacleGrid)):
+            if obstacleGrid[i][0] == 1:
+                break
+            else:
+                grid[i][0] = 1
+        for j in range(len(obstacleGrid[0])):
+            if obstacleGrid[0][j] == 1:
+                break
+            else:
+                grid[0][j] = 1
 
         for i in range(1, len(obstacleGrid)):
             for j in range(1, len(obstacleGrid[0])):
