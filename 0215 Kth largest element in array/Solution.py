5425e7034e303b4f36b7c29ba01b84da7206a4f4
Balazs <balazs@altaeros.com>
2022-08-04 14:39:41 -0400

    Add first solution to task 215

--- 0215 Kth largest element in array/Solution.py
+++ 0215 Kth largest element in array/Solution.py
@@ -1,3 +1,13 @@
+from queue import PriorityQueue
+
 class Solution:
     def findKthLargest(self, nums: list[int], k: int) -> int:
-        return -1
+        q = PriorityQueue()
+
+        for num in nums:
+            q.put(num * -1)
+
+        for i in range(k - 1):
+            q.get()
+
+        return q.get() * -1
