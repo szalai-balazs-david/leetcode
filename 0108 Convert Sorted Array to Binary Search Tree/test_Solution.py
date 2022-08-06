10e268eac268abb69a24fd176e7125c1c8037503
Balazs <balazs@altaeros.com>
2022-08-06 07:06:54 -0400

    Add baseline to task 108

---
+++ 0108 Convert Sorted Array to Binary Search Tree/test_Solution.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from Solution import Solution
+from Utilities.TreeNode import TreeNode, treeNode2List
+
+
+class TestSolution(TestCase):
+    def test_sorted_array_to_bst1(self):
+        x = Solution()
+        input = [-10,-3,0,5,9]
+        expected = [0,-3,9,-10,None,5, None]
+        actual = x.sortedArrayToBST(input)
+        self.assertListEqual(expected, actual)
+
+    def test_sorted_array_to_bst2(self):
+        x = Solution()
+        input = [1,3]
+        expected = [3,1, None]
+        actual = x.sortedArrayToBST(input)
+        self.assertListEqual(expected, actual)
