04df2a7bf623a94ffd08bc289173aa7e4cdf8c75
Balazs <balazs@altaeros.com>
2022-08-06 07:15:53 -0400

    Add first solution to task 108

--- 0108 Convert Sorted Array to Binary Search Tree/Solution.py
+++ 0108 Convert Sorted Array to Binary Search Tree/Solution.py
@@ -4,4 +4,18 @@ from typing import Optional
 
 class Solution:
     def sortedArrayToBST(self, nums: list[int]) -> Optional[TreeNode]:
-        return None
+        if nums is None:
+            return None
+        length = len(nums)
+        match length:
+            case 0:
+                return None
+            case 1:
+                return TreeNode(nums[0])
+            case 2:
+                return TreeNode(nums[1], TreeNode(nums[0]))
+            case 3:
+                return TreeNode(nums[1], TreeNode(nums[0]), TreeNode(nums[2]))
+        mid = length // 2
+
+        return TreeNode(nums[mid], self.sortedArrayToBST(nums[:mid]), self.sortedArrayToBST(nums[mid + 1:]))
--- 0108 Convert Sorted Array to Binary Search Tree/test_Solution.py
+++ 0108 Convert Sorted Array to Binary Search Tree/test_Solution.py
@@ -9,11 +9,11 @@ class TestSolution(TestCase):
         input = [-10,-3,0,5,9]
         expected = [0,-3,9,-10,None,5, None]
         actual = x.sortedArrayToBST(input)
-        self.assertListEqual(expected, actual)
+        self.assertListEqual(expected, treeNode2List(actual))
 
     def test_sorted_array_to_bst2(self):
         x = Solution()
         input = [1,3]
         expected = [3,1, None]
         actual = x.sortedArrayToBST(input)
-        self.assertListEqual(expected, actual)
+        self.assertListEqual(expected, treeNode2List(actual))
