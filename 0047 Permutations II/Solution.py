19a26efc156f3da3a04170d19796d1a6c0f5bbaa
Balazs <balazs@altaeros.com>
2022-08-08 10:03:25 -0400

    Add solution to task 47

--- 0047 Permutations II/Solution.py
+++ 0047 Permutations II/Solution.py
@@ -1,3 +1,25 @@
 class Solution:
     def permuteUnique(self, nums: list[int]) -> list[list[int]]:
-        return []
+
+        def backtrack(seq: list[int], result: list[list[int]], rest: list[int]):
+            if len(rest) == 0:
+                result.append(list(seq))
+                return
+
+            for i in range(len(rest)):
+                if i != rest.index(rest[i]):
+                    continue
+                val = rest[i]
+                seq.append(val)
+                del rest[i]
+                backtrack(seq, result, rest)
+                seq.pop()
+                rest.insert(i, val)
+
+        nums.sort()
+        results = []
+        seq = []
+
+        backtrack(seq, results, nums)
+
+        return results
