b81506cbfac228224e93eb7fbdbefe9dbd27207f
Balazs <balazs@altaeros.com>
2022-08-09 11:47:07 -0400

    Add baseline to task 61

---
+++ 0061 Rotate List/Solution.py
@@ -0,0 +1,7 @@
+from Utilities.ListNode import ListNode
+from typing import Optional
+
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        return None
---
+++ 0061 Rotate List/test_Solution.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from Solution import Solution
+from Utilities.ListNode import listNode2list, createListNode
+
+
+class TestSolution(TestCase):
+    def test_rotate_right1(self):
+        x = Solution()
+        expected = [4,5,1,2,3]
+        input = createListNode([1,2,3,4,5])
+        result = x.rotateRight(input, 2)
+        actual = listNode2list(result)
+        self.assertListEqual(expected, actual)
+
+    def test_rotate_right2(self):
+        x = Solution()
+        expected = [2,0,1]
+        input = createListNode([0,1,2])
+        result = x.rotateRight(input, 4)
+        actual = listNode2list(result)
+        self.assertListEqual(expected, actual)
