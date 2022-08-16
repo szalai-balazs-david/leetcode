191beb8f3ea9441f4babe7fc67bf769ff8e45f09
Balazs <balazs@altaeros.com>
2022-08-16 12:48:09 -0400

    Add accepted solution to task 123

--- 0123 Best Time to Buy and Sell Stock III/Solution.py
+++ 0123 Best Time to Buy and Sell Stock III/Solution.py
@@ -1,81 +1,79 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        class thingy:
-            def __init__(self, start: int, end: list[int], profit: list[int]):
-                self.start = start
-                self.end = end
-                self.profit = profit
 
-        def get_options() -> list[thingy]:
-            options = []
-            for i in range(len(prices) - 1):
-                if prices[i] == prices[i+1]:
-                    continue
-                current = thingy(i, [], [])
-                for j in range(i + 1, len(prices)):
-                    for k in range(len(current.end)):
-                        if prices[j] > prices[current.end[k]]:
-                            if current.end[k] == j-1:
-                                current.end[k] = j
-                                current.profit[k] = prices[j] - prices[current.start]
-                            else:
-                                current.end.append(j)
-                                current.profit.append(prices[j] - prices[i])
+        def get_max_profit(start: int, end: int) -> (int, int, int):
+            buy_index = start
+            best_buy = prices[buy_index]
+            buys = [best_buy]
+            buy_indexes = [buy_index]
+            for i in range(start + 1, end+1):
+                if prices[i] < best_buy:
+                    buy_index = i
+                    best_buy = prices[buy_index]
+                buys.append(best_buy)
+                buy_indexes.append(buy_index)
 
-                    if len(current.end) == 0:
-                        if prices[i] < prices[j]:
-                            current.end.append(j)
-                            current.profit.append(prices[j] - prices[i])
-                    else:
-                        if prices[i] + current.profit[-1] < prices[j]:
-                            current.end.append(j)
-                            current.profit.append(prices[j] - prices[i])
-                if len(current.end) > 0:
-                    options.append(current)
-            return options
+            sell_index = end
+            best_sell = prices[sell_index]
+            sells = [best_sell]
+            sell_indexes = [sell_index]
+            for i in range(end - 1, start - 1, -1):
+                if prices[i] >= best_sell:
+                    sell_index = i
+                    best_sell = prices[sell_index]
+                sells.append(best_sell)
+                sell_indexes.append(sell_index)
+            sells.reverse()
+            sell_indexes.reverse()
 
-        def filter_options(options: list[thingy]):
-            for i in range(len(options) - 1, 0, -1):
-                if options[i].start == options[i-1].start + 1 and options[i].profit[-1] < options[i-1].profit[-1]:
-                    options.remove(options[i])
-            #Remove same end & earlier start & lower profit
-            for i in range(len(options) - 1):
-                for ii in range(len(options[i].end) - 1, -1, -1):
-                    for j in range(i + 1, len(options)):
-                        done = False
-                        for jj in range(len(options[j].end)):
-                            start_is_earlier = options[i].start < options[j].start
-                            end_is_same = options[i].end[ii] == options[j].end[jj]
-                            profit_is_less = options[i].profit[ii] <= options[j].profit[jj]
-                            if start_is_earlier and end_is_same and profit_is_less:
-                                del options[i].end[ii]
-                                del options[i].profit[ii]
-                                done = True
-                                break
-                        if done:
-                            break
-            #remove options with no element
-            for i in range(len(options) - 1, -1, -1):
-                if len(options[i].profit) == 0:
-                    del options[i]
+            best_deal = 0
+            deal_index = -1
+            for i in range(len(buys)):
+                if sells[i] - buys[i] > best_deal:
+                    deal_index = i
+                    best_deal = sells[i] - buys[i]
+            return best_deal, buy_indexes[deal_index], sell_indexes[deal_index]
 
-        def calculate_profit(options: list[thingy]) -> int:
-            max_profit = 0
-            for i in range(len(options) - 1):
-                for ii in range(len(options[i].profit)):
-                    first_profit = options[i].profit[ii]
-                    for j in range(i + 1, len(options)):
-                        if options[j].start <= options[i].end[ii]:
-                            max_profit = max(max_profit, first_profit)
-                        else:
-                            profit = first_profit + options[j].profit[-1]
-                            max_profit = max(max_profit, profit)
-            return max_profit
+        length = len(prices)
+        #Deal with limited elements
+        match length:
+            case 1:
+                return 0
+            case 2:
+                return max(0, prices[1]-prices[0])
+            case 3:
+                return get_max_profit(0, 2)[0]
+            case 4:
+                if prices[1] > prices[2]:
+                    return get_max_profit(0,1)[0] + get_max_profit(2,3)[0]
+                else:
+                    return get_max_profit(0,3)[0]
 
-        options = get_options()
-        filter_options(options)
-        if len(options) == 0:
+        #More than 4 elements in the list
+        best_single_deal = get_max_profit(0, length-1)
+        max_profit = best_single_deal[0]
+
+        # Continuous price drop
+        if max_profit == 0:
             return 0
-        if len(options) == 1:
-            return options[0].profit[-1]
-        return calculate_profit(options)
+
+        divide = max(1, best_single_deal[1] - 1)
+        max_sell_value = max(prices)
+        while divide < length - 2:
+            first_deal = get_max_profit(0,divide)
+            second_deal = get_max_profit(divide+1, length-1)
+            max_profit = max(max_profit, first_deal[0] + second_deal[0])
+            divide = max(divide + 1, second_deal[1]-1)
+
+            # If second deal does not produce profit, not need to move the goalpost further
+            if second_deal[0] == 0:
+                break
+            # If first deal ends with the max sell price, there's no need to move the goalpost further
+            # Need to check if first deal actually produces profit. If not, the end time is invalid
+            if max_sell_value == prices[first_deal[2]] and first_deal[0] != 0:
+                break
+            if first_deal[2] + 1 == second_deal[1] and first_deal[0] != 0 and second_deal[0] != 0:
+                off_deal = get_max_profit(second_deal[1], second_deal[2] - 1)
+                divide = off_deal[2]
+
+        return max_profit
