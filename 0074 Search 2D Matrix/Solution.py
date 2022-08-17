c0c0b0fb4cad98b80b0e3d07e0bb8bc4066ab928
Balazs <balazs@altaeros.com>
2022-08-17 14:51:50 -0400

    Add recursive BS solution to task 74

--- 0074 Search 2D Matrix/Solution.py
+++ 0074 Search 2D Matrix/Solution.py
@@ -1,17 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        columns = len(matrix[0])
-        l = 0
-        r = columns * len(matrix)
-        while l < r:
-            mid = (l + r) // 2
-            element = matrix[mid // columns][mid % columns]
-            if element == target:
-                return True
-            if element < target:
-                if mid == l:
-                    return False
-                l = mid
+
+        def binary_search(low: int, high: int, column_count: int) -> bool:
+            # Check base case
+            if high >= low:
+
+                mid = (high + low) // 2
+                row = mid // column_count
+                column = mid % column_count
+
+                # If element is present at the middle itself
+                if matrix[row][column] == target:
+                    return True
+
+                # If element is smaller than mid, then it can only
+                # be present in left subarray
+                elif matrix[row][column] > target:
+                    return binary_search(low, mid - 1, column_count)
+
+                # Else the element can only be present in right subarray
+                else:
+                    return binary_search(mid + 1, high, column_count)
+
             else:
-                r = mid
-        return False
+                # Element is not present in the array
+                return False
+
+        columns = len(matrix[0])
+        elements = len(matrix) * columns
+        return binary_search(0, elements - 1, columns)
