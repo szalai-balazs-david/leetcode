8eefd86cb365d7ab136010fc554ad5ac3f63d880
Balazs <balazs@altaeros.com>
2022-08-30 09:29:39 -0400

    Add improved own solution with backtracking. Beats 86/74

--- 0473 Matchsticks to Square/Solution.py
+++ 0473 Matchsticks to Square/Solution.py
@@ -1,23 +1,56 @@
+from collections import Counter
+
+
 class Solution:
+    def __init__(self):
+        self.skips = 0
+
     def makesquare(self, matchsticks: list[int]) -> bool:
         total = sum(matchsticks)
         if total % 4 != 0:
             return False
         target = total // 4
 
-        matchsticks.sort()
-        if matchsticks[0] > target:
-            return False
-        
-        def backtrack(sums: list[int], index: int):
-            for i in range(4):
-                if sums[i] + matchsticks[index] <= target:
-                    sums[i] += matchsticks[index]
-                    if index == 0:
-                        return True
-                    if backtrack(sums, index - 1):
-                        return True
-                    sums[i] -= matchsticks[index]
+        counter = Counter(matchsticks)
+        unique = list(counter.keys())
+        unique.sort()
+
+        if max(unique) > target:
             return False
 
-        return backtrack([0,0,0,0], len(matchsticks) - 1)
+        def backtrack(remainder: int, index: int) -> bool:
+            while index >= 0:
+                if unique[index] > remainder or counter[unique[index]] <= 0:
+                    index -= 1
+                    continue
+                remainder -= unique[index]
+                counter[unique[index]] -= 1
+                if remainder == 0:
+                    if self.skips == 0:
+                        return True
+                    self.skips -= 1
+                    remainder += unique[index]
+                    counter[unique[index]] += 1
+                    return False
+                if backtrack(remainder, index):
+                    return True
+                remainder += unique[index]
+                counter[unique[index]] += 1
+                index -= 1
+            return False
+
+        skip_counter = 0
+        while True:
+            success = True
+            self.skips = skip_counter
+            counter = Counter(matchsticks)
+            for i in range(4):
+                print(counter)
+                if not backtrack(target, len(unique) - 1):
+                    success = False
+                    break
+            if success:
+                return True
+            if self.skips != 0:
+                return False
+            skip_counter += 1
