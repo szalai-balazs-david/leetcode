6869ff0d5778a65ce5bbf14f2cac48677ee5101c
Balazs <balazs@altaeros.com>
2022-08-31 10:00:06 -0400

    Add shorter code to task 1146. Cheated. This came from forum

--- 1146 Snapshot Array/Solution.py
+++ 1146 Snapshot Array/Solution.py
@@ -1,53 +1,20 @@
+import bisect
+
+
 class SnapshotArray:
 
+    # Cheated. Looked at solution. But this bisect is helpful!
     def __init__(self, length: int):
         self.index = 0
-        self.changes = [{} for sub in range(length)]
+        self.changes = [[[-1, 0]] for i in range(length)]
 
     def set(self, index: int, val: int) -> None:
-        self.changes[index][self.index] = val
+        self.changes[index].append([self.index, val])
 
     def snap(self) -> int:
         self.index += 1
         return self.index - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        def find_closest_lower(keys: list[int], target: int) -> int:
-            keys.sort()
-
-            # Looking for latest value
-            if keys[-1] <= target:
-                return keys[-1]
-            # No change was made yet when snapshot was made
-            if keys[0] > target:
-                return -1
-
-            left = 0
-            right = len(keys) - 2
-            while left < right:
-                mid = left + (right - left) // 2
-                if keys[mid] <= target and keys[mid + 1] > target:
-                    return keys[mid]
-                elif keys[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-
-            return keys[left]
-
-        element_history = self.changes[index]
-
-        # No change was made to this element
-        if len(element_history) == 0:
-            return 0
-
-        # We have the exact snapshot
-        if snap_id in element_history:
-            return element_history[snap_id]
-
-        # No snapshot of that moment. Have to look for latest previous change
-        ids = list(element_history.keys())
-        actual_snap_id = find_closest_lower(ids, snap_id)
-        if actual_snap_id == -1:
-            return 0
-        return element_history[actual_snap_id]
+        i = bisect.bisect(self.changes[index], [snap_id + 1]) - 1
+        return self.changes[index][i][1]
