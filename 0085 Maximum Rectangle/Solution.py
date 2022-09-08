161e3e2c99f9592fccb5eb41ccd57e862163b10b
Balazs <balazs@altaeros.com>
2022-09-08 18:26:28 -0400

    Add faster solution. Still exceeds time limit, but with bigger dataset.
    Cheated. Took a quick look at a solution in the discussion and immediately got an idea on how to do it better...

--- 0085 Maximum Rectangle/Solution.py
+++ 0085 Maximum Rectangle/Solution.py
@@ -1,37 +1,26 @@
 class Solution:
     def maximalRectangle(self, matrix: list[list[str]]) -> int:
-        column_count = len(matrix[0])
-        values = [[0] * column_count for i in range(column_count)]
-        result = 0
-        for row in matrix:
-            start = None
-            for j in range(len(row)):
-                if row[j] == "1":
-                    # Add all options between oldest 1 to this 1 to values
-                    if start is None:
-                        start = j
-                        values[j][j] += 1
-                    else:
-                        # There was at least 1 one before this. Add all options ending with this node
-                        for start_index in range(start, j + 1):
-                            values[start_index][j] += 1
-                else:
-                    if start is None:
-                        for end_index in range(j, column_count):
-                            result = max(result, values[j][end_index] * (end_index - j + 1))
-                            values[j][end_index] = 0
-                    else:
-                        # Remove options that are no longer valid, and evaluate result
-                        for start_index in range(start, column_count):
-                            for end_index in range(start_index, column_count):
-                                if start_index <= j <= end_index and values[start_index][end_index] != 0:
-                                    result = max(result, values[start_index][end_index] * (end_index - start_index + 1))
-                                    values[start_index][end_index] = 0
-                    # Lastly mark that no 1 was found prior
-                    start = None
+        rows = len(matrix)
+        columns = len(matrix[0])
+        values = [[0] * columns for i in range(rows)]
+        for i in range(rows):
+            for j in range(columns):
+                values[i][j] = 0 if matrix[i][j] == "0" else (values[i][j-1] + 1) if j > 0 else 1
 
-        for start_index in range(column_count):
-            for end_index in range(start_index, column_count):
-                if values[start_index][end_index] != 0:
-                    result = max(result, values[start_index][end_index] * (end_index - start_index + 1))
+        result = 0
+        for i in range(rows - 1, -1, -1):
+            for j in range(columns):
+                if values[i][j] == 0:
+                    continue
+                width = values[i][j]
+                current_best = 0
+                for k in range(i, -1, -1):
+                    width = min(width, values[k][j])
+                    # Optimize break condition
+                    if width == 0:
+                        break
+                    current_best = max(current_best, (i - k + 1) * width)
+                values[i][j] = current_best
+                result = max(result, current_best)
+
         return result
