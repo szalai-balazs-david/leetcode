946bb426d323c55ca1dd31ab368f271b3bbb8c87
Balazs <balazs@altaeros.com>
2022-09-08 18:34:55 -0400

    Add accepted solution to task 85

--- 0085 Maximum Rectangle/Solution.py
+++ 0085 Maximum Rectangle/Solution.py
@@ -15,11 +15,14 @@ class Solution:
                 width = values[i][j]
                 current_best = 0
                 for k in range(i, -1, -1):
-                    width = min(width, values[k][j])
-                    # Optimize break condition
-                    if width == 0:
-                        break
-                    current_best = max(current_best, (i - k + 1) * width)
+                    # Only do calc if width decreased
+                    if values[k][j] < width:
+                        current_best = max(current_best, (i - k) * width)
+                        width = values[k][j]
+                        # If there's no way to beat current_best with this width, stop looping
+                        if width * (i + 1) <= current_best:
+                            break
+                current_best = max(current_best, (i + 1) * width)
                 values[i][j] = current_best
                 result = max(result, current_best)
 
